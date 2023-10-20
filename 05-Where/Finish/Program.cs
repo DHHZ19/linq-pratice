e9d0b54c414b24c2a9f2a7871fc395694d293f07
Diego Hernandez <diegofootball19@gmail.com>
2023-10-20 12:29:51 -0500

    first commit

---
+++ 05-Where/Finish/Program.cs
@@ -0,0 +1,30 @@
+﻿using LINQSamples;
+
+// Create instance of view model
+SamplesViewModel vm = new();
+
+// Call Sample Method
+var result = vm.WhereMethod();
+
+List<List<int>> listOfLists = new List<List<int>>
+{
+    new List<int> { 1, 2, 3 },
+    new List<int> { 4, 5, 6 },
+    new List<int> { 7, 8, 9 }
+};
+
+var i = 0;
+foreach (var innerlist in listOfLists)
+{
+    foreach (var num in innerlist)
+    {
+        if (i <= 4)
+        {
+            i++;
+            Console.WriteLine(i);
+        }
+    }
+}
+
+// Display Results
+//vm.Display(result);
